afafb3c1b802c825120176fa5d6e483aa574ae4b
Alistair A. Israel <aisrael@gmail.com>
2018-03-17 08:12:32 +0800

    Avoid SQL injection in CreateTable

--- spec/cql/command/create_table_spec.cr
+++ spec/cql/command/create_table_spec.cr
@@ -9,4 +9,27 @@ describe CQL::Command::CreateTable do
     end
     sql.should eq("CREATE TABLE foobar (id SERIAL);")
   end
+  it "raises an ArgumentError if the table name is invalid" do
+    expect_raises(ArgumentError) do
+      CQL::Command::CreateTable.new(CQL.postgres, "")
+    end.message.should eq(%(Invalid table name ""))
+    expect_raises(ArgumentError) do
+      CQL::Command::CreateTable.new(CQL.postgres, "_starts_with_underscore")
+    end.message.should eq(%(Invalid table name "_starts_with_underscore"))
+    expect_raises(ArgumentError) do
+      CQL::Command::CreateTable.new(CQL.postgres, "1starts_with_digit")
+    end.message.should eq(%(Invalid table name "1starts_with_digit"))
+    expect_raises(ArgumentError) do
+      CQL::Command::CreateTable.new(CQL.postgres, "contains'singlequote")
+    end.message.should eq(%(Invalid table name "contains'singlequote"))
+    expect_raises(ArgumentError) do
+      CQL::Command::CreateTable.new(CQL.postgres, %(contains"doublequote))
+    end.message.should eq(%(Invalid table name "contains"doublequote"))
+    expect_raises(ArgumentError) do
+      CQL::Command::CreateTable.new(CQL.postgres, "contains,comma")
+    end.message.should eq(%(Invalid table name "contains,comma"))
+    expect_raises(ArgumentError) do
+      CQL::Command::CreateTable.new(CQL.postgres, "contains;semicolon")
+    end.message.should eq(%(Invalid table name "contains;semicolon"))
+  end
 end
--- src/cql/command/create_table.cr
+++ src/cql/command/create_table.cr
@@ -2,12 +2,15 @@ struct CQL::Command::CreateTable < CQL::Command
   getter :table_name
   getter :columns
 
+  VALID_TABLE_NAME_PATTERN = /^[[:alpha:]][[:alpha:]0-9_]+$/
   def initialize(@database : CQL::Database, @table_name : String)
+    raise ArgumentError.new(%(Invalid table name "#{@table_name}")) unless @table_name =~ VALID_TABLE_NAME_PATTERN
     super(@database)
     @columns = [] of CQL::Column
   end
 
   def initialize(@database : CQL::Database, table : CQL::Table)
+    initialize(@database, table.name)
     @table_name = table.name
     @columns = table.columns
   end
@@ -26,12 +29,13 @@ struct CQL::Command::CreateTable < CQL::Command
     self
   end
   def exec
-    sql = self.to_s
+    sql = to_s
     debug sql
     @database.with_db do |db|
-      db.exec(self.to_s)
+      db.exec(sql)
     end
   end
+
   # TODO: Move to CQL::Dialect?
   def to_s(io)
     io << "CREATE TABLE #{table_name} ("
