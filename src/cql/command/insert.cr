5bf2bd8b47c2f7c4982304d041e01d8589537a7d
Alistair A. Israel <aisrael@gmail.com>
2018-07-16 14:57:12 +0800

    Insert is now immutable

--- src/cql/command/insert.cr
+++ src/cql/command/insert.cr
@@ -2,32 +2,24 @@ struct CQL::Command::Insert < CQL::Command
   getter :table_name
   getter :column_names
 
-  @column_names = [] of String
-
-  def initialize(@database : CQL::Database, @table_name : String)
+  def initialize(@database : CQL::Database, @table_name : String, @column_names = [] of String)
     super(@database)
   end
 
   def column(name : String)
-    @column_names << name
-    self
+    new_column_names = @column_names + [name]
+    Insert.new(@database, @table_name, new_column_names)
   end
 
   def columns(column_names : Array(String))
-    @column_names += column_names
-    self
+    new_column_names = @column_names + column_names
+    Insert.new(@database, @table_name, new_column_names)
   end
 
-  def columns(*args : String)
-    args.each do |arg|
-      case arg
-      when String
-        @column_names << arg
-      else
-        raise "Don't know how to handle column name of type #{arg.class}!"
-      end
-    end
-    self
+  def columns(*column_names : String)
+    new_column_names = [] of String + @column_names
+    column_names.each { |column_name| new_column_names << column_name }
+    Insert.new(@database, @table_name, new_column_names)
   end
 
   def exec(args : Array(U)) forall U
