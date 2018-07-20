f232c694bdab336e3cdd507cda4fa3a88b82028b
Alistair A. Israel <aisrael@gmail.com>
2018-07-20 15:48:13 +0800

    Use File.join(__DIR__, ...

--- etc/read_as_hash.cr
+++ etc/read_as_hash.cr
@@ -1,7 +1,7 @@
 list = ARGV[0]
 
 puts "Hash{"
-lines = File.each_line("./etc/#{list}.txt").map do |line|
+lines = File.each_line(File.join(__DIR__, "#{list}.txt")).map do |line|
   singular, plural = line.split(/:\s+/)
   %(  "#{singular}" => "#{plural}")
 end
