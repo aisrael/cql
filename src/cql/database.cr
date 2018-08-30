d448bed5b78251ad369b0bd5bf7d4b78ecb720d4
Alistair A. Israel <aisrael@gmail.com>
2018-08-31 04:29:49 +0800

    Try logging 0.2

--- src/cql/database.cr
+++ src/cql/database.cr
@@ -59,30 +59,54 @@ abstract class CQL::Database
   end
 
   def exec(sql, *args) : DB::ExecResult
-    debug(sql)
+    debug {
+      inject_args(sql, *args)
+    }
     db.exec(sql, *args)
   end
 
   def scalar(sql, *args)
-    debug(sql)
+    debug {
+      inject_args(sql, *args)
+    }
     db.scalar(sql, *args)
   end
 
   # Directly query for one record and map it (really just delegates to the internal @db)
   def query_one(sql, *args, &block : DB::ResultSet -> U) : U forall U
-    debug(sql)
+    debug {
+      inject_args(sql, *args)
+    }
     db.query_one(sql, *args, &block)
+  end
+
+  # TODO: Move somewhere else?
+  private def inject_args(sql, *args)
+    sql.gsub(/\$[0-9]+/) do |s|
+      offset = s[1..-1].to_i
+      val = args[offset - 1]
+      case val
+      when String
+        "'#{val}'"
+      else
+        val.to_s
+      end
+    end
   end
 
   # Directly query for zero or one records and map it (really just delegates to the internal @db)
   def query_one?(sql, *args, &block : DB::ResultSet -> U) : U | Nil forall U
-    debug(sql)
+    debug do
+      inject_args(sql, *args)
+    end
     db.query_one?(sql, *args, &block)
   end
 
   # Directly query for multiple records and map them (really just delegates to the internal @db)
   def query_all(sql, *args, &block : DB::ResultSet -> U) : Array(U) forall U
-    debug(sql)
+    debug {
+      inject_args(sql, *args)
+    }
     db.query_all(sql, *args, &block)
   end
 end
